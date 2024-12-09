841abc249c3b3ce0d5652cfa33aca105278ab0b9
VesztegBernadett <vdetti2004@gmail.com>
2024-12-09 12:30:11 +0000

    kész

--- backend/app/Http/Controllers/PandaController.php
+++ backend/app/Http/Controllers/PandaController.php
@@ -9,8 +9,34 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PandaController extends Controller
 {
-    function index():JsonResource{
-        return PandaResource::collection(Panda::all());
+    function index(Request $request):JsonResource{
+        $pandas = Panda::all();
+
+        $orderBy = $request->query("orderBy");
+        $order = $request->query("order");
+
+        if (!($orderBy=="name"||$orderBy=="age"||$orderBy==null)||!($order=="asc"||$order=="desc"||$order==null)) {
+            abort(404);
+        }
+        
+        if ($orderBy=="name") {
+            if ($order=="desc") {
+                $pandas = $pandas->sortByDesc("name");
+            }
+            else{
+                $pandas = $pandas->sortBy("name");
+            }
+        }
+        elseif ($orderBy=="age") {
+            if ($order=="desc") {
+                $pandas = $pandas->sortByDesc("birth");
+            }
+            else{
+                $pandas = $pandas->sortBy("birth");
+            }
+        }
+
+        return PandaResource::collection($pandas);
     }
 
     function show(int $id):JsonResource{
